7fd7c1f882b270a8ec0cd078e2ea53c79b0065b5
Noah Muth <noahmuth@gmail.com>
2018-01-18 09:30:07 -0600

    Take in extra options in blockstack funcs

--- src/services/blockstack.js
+++ src/services/blockstack.js
@@ -1,25 +1,25 @@
 import * as blockstack from 'blockstack'
 
-export const getJson = async (filename, { username = null } = {}) => {
+export const getJson = async (filename, { username = null, ...options } = {}) => {
   if (!username) {
-    return JSON.parse(await blockstack.getFile(filename, { decrypt: true }))
+    return JSON.parse(await blockstack.getFile(filename, { decrypt: true, ...options }))
   }
 
   console.debug(`reading ${filename} from user ${username}`)
-  const options = {
+  const publicOptions = {
     username,
     app: 'http://localhost:3000' // TODO put this in configuration instead of a constant
-    //app: 'https://hihermes.co'
+    //app: 'https://hihermes.co',
   }
-  return JSON.parse(await blockstack.getFile(filename, options))
+  return JSON.parse(await blockstack.getFile(filename, { ...publicOptions, ...options }))
 }
 
-export const saveJson = (filename, json, { isPublic = false } = {}) => {
+export const saveJson = (filename, json, { isPublic = false, ...options } = {}) => {
   if (isPublic) {
     console.debug(`saving to ${filename} [PUBLIC FILE]:`, json)
-    return blockstack.putFile(filename, JSON.stringify(json), { encrypt: false })
+    return blockstack.putFile(filename, JSON.stringify(json), { encrypt: false, ...options })
   }
 
   console.debug(`saving to ${filename}:`, json)
-  return blockstack.putFile(filename, JSON.stringify(json), { encrypt: true })
+  return blockstack.putFile(filename, JSON.stringify(json), { encrypt: true, ...options })
 }
