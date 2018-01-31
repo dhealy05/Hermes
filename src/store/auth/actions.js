e6f2f07725a183ea442bc9e02a09f14c0e66282c
Noah Muth <noahmuth@gmail.com>
2018-01-30 18:45:51 -0600

    https for blockstack core

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -10,7 +10,7 @@ export const checkAuth = dispatch => {
   if (blockstack.isUserSignedIn()) {
     dispatch(setIdentity(identity()))
   } else if (blockstack.isSignInPending()) {
-    blockstack.handlePendingSignIn('http://core.blockstack.org/v1/names/')
+    blockstack.handlePendingSignIn('https://core.blockstack.org/v1/names/')
       .then(userData => {
         window.location = window.location.origin
       })
