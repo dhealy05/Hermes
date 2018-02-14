d68c1875461159acd436e0bce823aad83c844949
Noah Muth <noahmuth@gmail.com>
2018-02-14 14:06:34 -0600

    Fix image float bug

--- src/components/Message.js
+++ src/components/Message.js
@@ -23,15 +23,16 @@ const Content = styled.div`
 
 const ImageMessage = styled.img`
   max-width: 66%;
-  float: right;
   padding: 2px;
   margin-top: 3px;
   border-radius: 2px 8px 8px 2px;
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.2) 0 1px 1px 0;
+  float: left;
 
   ${props => ((props.direction === 'right') && css`
     border-radius: 8px 2px 2px 8px;
+    float: right;
   `)}
 `
 
