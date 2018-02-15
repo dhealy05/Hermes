e608e453625f4ddae43a0efff5c4775387bd898a
Noah Muth <noahmuth@gmail.com>
2018-02-15 10:28:02 -0600

    Fix notifications permission flow

--- src/services/notify.js
+++ src/services/notify.js
@@ -3,18 +3,26 @@ export const notify = async (title, options) => {
     throw new Error('must specify title when creating a notification')
   }
 
-  if (!('Notification' in window) || document.hasFocus()) {
+  if (!('Notification' in window) || Notification.permission === 'denied') {
+    // exit if we're not allowed to send notifications
     return null
   }
 
   if (Notification.permission !== 'granted') {
+    // ask for permission if we haven't done that yet
     await Notification.requestPermission()
   }
 
   if (Notification.permission !== 'granted') {
+    // now that we've asked for permission, exit if we got denied or ignored
     return null
   }
 
+  if (document.hasFocus()) {
+    // don't show notifications if the user is looking at hermes
+    return null
+  }
+  
   const notification = new Notification(title, options)
   setTimeout(() => notification.close(), 5000)
   return notification
